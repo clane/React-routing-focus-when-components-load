4620deb3d8a9491a0ade04343aefa7642c009c5d
Chris Lane <chris.lane359@gmail.com>
2018-12-06 08:51:59 -0800

    now using individual properties to set aria-current for each router link

--- app/src/App.js
+++ app/src/App.js
@@ -8,6 +8,20 @@ import { Helmet } from "react-helmet";
 import { BrowserRouter, Route, Switch, Link, Redirect } from "react-router-dom";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    // Create a ref to store the textInput DOM element
+    this.link1 = React.createRef();
+    this.link2 = React.createRef();
+    this.link3 = React.createRef();
+    this.link4 = React.createRef();
+
+    this.link1Current = "false";
+    this.link2Current = "false";
+    this.link3Current = "false";
+    this.link4Current = "false";
+  }
+
   render() {
     return (
       <div className="App">
@@ -30,10 +44,43 @@ class App extends Component {
           <div>
 
             <ul role="navigation">
-              <li><Link to="Component1">Component 1</Link></li>
-              <li><Link to="Component2">Component 2</Link></li>
-              <li><Link to="Component3">Component 3</Link></li>
-              <li><Link to="Component4">Component 4</Link></li>
+              <li>
+                <Link
+                  aria-current={this.link1Current}
+                  ref={this.link1}
+                  to="Component1"
+                >
+                  Component 1
+                </Link>
+              </li>
+              <li>
+                <Link
+                  aria-current={this.link2Current}
+                  ref={this.link2}
+                  to="Component2"
+                >
+                  Component 2
+                </Link>
+              </li>
+              <li>
+                <Link
+                  aria-current={this.link3Current}
+                  ref={this.link3}
+                  to="Component3"
+                >
+                  Component 3
+                </Link>
+              </li>
+              <li>
+                <Link
+                  aria-current={this.link4Current}
+                  ref={this.link4}
+                  to="Component4"
+                >
+                  Component 4
+                </Link>
+              </li>
+
             </ul>
 
             <Switch>
@@ -41,8 +88,8 @@ class App extends Component {
               <Route path="/Component2" component={Component2} />
               <Route path="/Component3" component={Component3} />
               <Route path="/Component4" component={Component4} />
-							{/*<Redirect from="/" to="/Component1" />*/}
-							<Redirect from="/" to="/" />
+              {/*<Redirect from="/" to="/Component1" />*/}
+              <Redirect from="/" to="/" />
             </Switch>
           </div>
         </BrowserRouter>
