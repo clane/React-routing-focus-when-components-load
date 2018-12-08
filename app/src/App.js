0e33895e6e3e4f527b3eb65df2e19d3f8eca0a11
Chris Lane <chris.lane359@gmail.com>
2018-12-08 07:04:48 -0800

    adding a label to navigation region

--- app/src/App.js
+++ app/src/App.js
@@ -76,7 +76,7 @@ class App extends Component {
         <BrowserRouter>
           <div>
 
-            <ul role="navigation">
+            <ul role="navigation" aria-label="primary navigation">
 
               <li>
                 <Link
