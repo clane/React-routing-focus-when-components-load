f91229af3efe05c901c7b18df3b6e81b61e8b7e6
Chris Lane <chris.lane359@gmail.com>
2018-12-09 12:44:02 -0800

    Added relative width to root and added a function for the iE11 polyfill

--- app/src/App.js
+++ app/src/App.js
@@ -23,6 +23,8 @@ class App extends Component {
     this.link4 = React.createRef();
 
     this.ariaCurrentSetter = this.setCurrentRouterLink.bind(this);
+
+    this.ie11Polyfill();
   }
 
   setCurrentRouterLink(componentName) {
@@ -51,6 +53,41 @@ class App extends Component {
     if (componentName === "component4") {
       this.setState({
         ariaCurrent4: "page"
+      });
+    }
+  }
+
+  ie11Polyfill() {
+    if (typeof Object.assign != "function") {
+      // Must be writable: true, enumerable: false, configurable: true
+      Object.defineProperty(Object, "assign", {
+        value: function assign(target, varArgs) {
+          // .length of function is 2
+          "use strict";
+          if (target == null) {
+            // TypeError if undefined or null
+            throw new TypeError("Cannot convert undefined or null to object");
+          }
+
+          var to = Object(target);
+
+          for (var index = 1; index < arguments.length; index++) {
+            var nextSource = arguments[index];
+
+            if (nextSource != null) {
+              // Skip over if undefined or null
+              for (var nextKey in nextSource) {
+                // Avoid bugs when hasOwnProperty is shadowed
+                if (Object.prototype.hasOwnProperty.call(nextSource, nextKey)) {
+                  to[nextKey] = nextSource[nextKey];
+                }
+              }
+            }
+          }
+          return to;
+        },
+        writable: true,
+        configurable: true
       });
     }
   }
